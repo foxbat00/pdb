dca4da44087507a96aa4d1e83e9e6428ec7aed47
Foxbat <foxbat00@gmail.com>
2014-02-17 00:23:45 -0800

    working search

--- app/browse/views.py
+++ app/browse/views.py
@@ -7,6 +7,7 @@ from sqlalchemy import *
 from app.browse.forms import BrowseForm
 import json
 from functools import wraps
+from app.browse.decorators import xpjax_only
 
 
 
@@ -44,65 +45,20 @@ def lvdict(labelsvalues):
 	
 
 
-#### Decorator for view functions to check for PJAX header
-  ## returns 403 error if PJAX header not detected
-
-def xpjax_only(view_function):
-    @wraps(view_function)
-    # the new, post-decoration function. Note *args and **kwargs here.
-    def decorated_function(*args, **kwargs):
-	if "X-PJAX" in request.headers:
-            return view_function(*args, **kwargs)
-        else:
-	    app.logger.error('ERROR:  non-PJAX request to PJAX route')
-            abort(403)
-    return decorated_function
-
-
-@mod.route('/browse/', methods=('GET',) )
-def browse_view():
-    return render_template('browse.html')
-
-
-#### PJAX ####
-
 @mod.route('/browse/', methods=('GET','POST') )
-@xpjax_only		    # route must be outermost to work correctly
-def search_query(query):
-    max = 100
-    query = request.args.get('query')
-    afd = active_files('display_name')
-    q = session.query(afd.c.display_name)\
-	.filter( afd.c.display_name.ilike('%'+query+'%') )
-    rs = q.limit(max).all()
-    count = len(q.all())
-    return render_template('pjax/results.html', results=sfrToList(rs), count=count, max=max)
-
-
-
-
-
-
-
-#### AJAX  ####
-
-"""
-@mod.route('/browse/_search')
-def ajax_search():
-    search = request.args.get('term')
-    afd = active_files('id','display_name')
-    rs = session.query(afd.c.display_name, afd.c.id)\
-	.filter( afd.c.display_name.ilike('%'+search+'%') )\
-	.limit(2).all()
-    if rs:
-	js = lvdict(rs)
-	app.logger.debug("returning...%d\n\n %s" % (len(rs), js))
-	return json.dumps(js)
-    else:
-	return None
-
-"""
-    
-
-
+def browse_view():
+    app.logger.debug("in browse_view")
+    if "X-PJAX" in request.headers:
+	app.logger.debug("xpjax detected ")
+	if "query" in request.args:
+	    query = request.args.get('query')
+	    app.logger.debug("query = #%s#" % query)
+	    max = 100
+	    afd = active_files('display_name')
+	    q = session.query(afd.c.display_name)\
+		.filter( afd.c.display_name.ilike('%'+query+'%') )
+	    rs = q.limit(max).all()
+	    count = len(q.all())
+	    return render_template('pjax/results.html', results=sfrToList(rs), count=count, max=max)
+    return render_template('browse.html')
 
